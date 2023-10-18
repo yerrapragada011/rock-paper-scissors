c7b86869d855937802582c56a2ce7140cd134907
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2023-10-18 13:42:11 -0400

    Add buttons for UI

--- script.js
+++ script.js
@@ -9,44 +9,85 @@ function getComputerChoice() {
 let playerScore = 0
 let computerScore = 0
 
+const rockBtn = document.querySelector('#rock')
+const paperBtn = document.querySelector('#paper')
+const scissorsBtn = document.querySelector('#scissors')
+const results = document.querySelector('#results')
+const finalResults = document.querySelector('#finalResults')
+const yourScoreCount = document.querySelector('#yourScore')
+const computerScoreCount = document.querySelector('#computerScore')
+
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    console.log('Tie game!')
+    results.innerHTML = 'Tie game!'
   } else if (playerSelection === 'rock' && computerSelection === 'paper') {
     computerScore++
-    console.log('You lose! Paper beats rock')
+    yourScoreCount.innerHTML = 'Your score is: ' + playerScore
+    computerScoreCount.innerHTML = 'The computer score is: ' + computerScore
+    results.innerHTML = 'Paper beats rock!'
   } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
     playerScore++
-    console.log('You win! Rock beats scissors')
+    yourScoreCount.innerHTML = 'Your score is: ' + playerScore
+    computerScoreCount.innerHTML = 'The computer score is: ' + computerScore
+    results.innerHTML = 'Rock beats scissors!'
   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
     playerScore++
-    console.log('You win! Paper beats rock')
+    yourScoreCount.innerHTML = 'Your score is: ' + playerScore
+    computerScoreCount.innerHTML = 'The computer score is: ' + computerScore
+    results.innerHTML = 'Paper beats rock!'
   } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
     computerScore++
-    console.log('You lose! Scissors beats paper')
+    yourScoreCount.innerHTML = 'Your score is: ' + playerScore
+    computerScoreCount.innerHTML = 'The computer score is: ' + computerScore
+    results.innerHTML = 'Scissors beats paper!'
   } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
     computerScore++
-    console.log('You lose! Rock beats scissors')
-  } else {
+    yourScoreCount.innerHTML = 'Your score is: ' + playerScore
+    computerScoreCount.innerHTML = 'The computer score is: ' + computerScore
+    results.innerHTML = 'Rock beats scissors!'
+  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
     playerScore++
-    console.log('You win! Scissors beats paper')
+    yourScoreCount.innerHTML = 'Your score is: ' + playerScore
+    computerScoreCount.innerHTML = 'The computer score is: ' + computerScore
+    results.innerHTML = 'Scissors beats paper!'
+  }
+}
+
+function calculateScore() {
+  if (playerScore == 5) {
+    finalResults.innerHTML = 'You win!'
+    rockBtn.disabled = true
+    paperBtn.disabled = true
+    scissorsBtn.disabled = true
+  } else if (computerScore == 5) {
+    finalResults.innerHTML = 'You lose!'
+    rockBtn.disabled = true
+    paperBtn.disabled = true
+    scissorsBtn.disabled = true
+  } else if (playerScore == 0 || computerScore == 0) {
+    finalResults.innerHTML = ''
   }
 }
 
 function game() {
-  const playerSelection = prompt('Rock, Paper, Scissors').toLowerCase()
-  const computerSelection = getComputerChoice()
-  playRound(playerSelection, computerSelection)
+  rockBtn.addEventListener('click', () => {
+    let playerSelection = 'rock'
+    let computerSelection = getComputerChoice()
+    playRound(playerSelection, computerSelection)
+    calculateScore()
+  })
+  paperBtn.addEventListener('click', () => {
+    let playerSelection = 'paper'
+    let computerSelection = getComputerChoice()
+    playRound(playerSelection, computerSelection)
+    calculateScore()
+  })
+  scissorsBtn.addEventListener('click', () => {
+    let playerSelection = 'scissors'
+    let computerSelection = getComputerChoice()
+    playRound(playerSelection, computerSelection)
+    calculateScore()
+  })
 }
 
 game()
-game()
-game()
-game()
-game()
-
-if (playerScore > computerScore) {
-    console.log('You win!')
-} else {
-    console.log('You lose!')
-}
